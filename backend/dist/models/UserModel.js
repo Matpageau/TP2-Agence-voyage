7ce39d41e809b5004b4971e9503841d1efd20aab
William Ferguson <willfergusmusic@gmail.com>
2025-08-05 12:57:05 -0400

    Dist

---
+++ backend/dist/models/UserModel.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const UserSchema = new mongoose_1.default.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "manager", "admin"]
+    },
+    liked: {
+        type: [String],
+        default: []
+    }
+});
+const UserModel = mongoose_1.default.model("users", UserSchema);
+exports.default = UserModel;
