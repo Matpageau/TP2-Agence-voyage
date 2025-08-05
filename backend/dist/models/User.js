7fb760935bf3238f1de2c30e5124d76eac99b154
MatPageau <maladarix@gmail.com>
2025-08-05 12:53:17 -0400

    dist

--- backend/dist/models/User.js
+++ backend/dist/models/User.js
@@ -30,8 +30,8 @@ class User {
         }
         return null;
     }
-    static getAll() {
-        return UserModel_1.default.find();
+    static getAll(max) {
+        return UserModel_1.default.find().limit(max);
     }
     static getById(id) {
         if (!mongoose_1.default.Types.ObjectId.isValid(id)) {
