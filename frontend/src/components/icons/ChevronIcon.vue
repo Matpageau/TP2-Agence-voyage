5d6dad8add3d6f5f00c3765d2ed70df7ae2df5c8
William Ferguson <willfergusmusic@gmail.com>
2025-08-07 22:29:07 -0400

    PageNav start

---
+++ frontend/src/components/icons/ChevronIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  direction?: string
+}>()
+</script>
+
+<template>
+  <svg v-if="props.direction === 'left'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+  </svg>
+  <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+  </svg>
+</template>
---
+++ frontend/src/components/shared/Buttons/PageNav.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ChevronIcon from '@/components/icons/ChevronIcon.vue';
+</script>
+
+<template>
+  <div class="flex items-center gap-5">
+    <ChevronIcon direction="left" />
+    <div class="grid grid-cols-5 items-center gap-4 text-lg">
+      <p>1</p>
+      <p>2</p>
+      <p>...</p>
+      <p>9</p>
+      <p>10</p>
+    </div>
+    <ChevronIcon />
+  </div>
+</template>
